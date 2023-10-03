8f02792f35819cc92ed31eb47fd9c332ab19e5f8
michal mechlovich <michali10us@gmail.com>
2023-10-03 14:01:18 -0700

    adding axious

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Checking to see if it works</p>
+        <p>Cheack the weather</p>
         <Weather />
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,55 @@
-import React from "react";
+import { upload } from "@testing-library/user-event/dist/upload";
+import { hasFormSubmit } from "@testing-library/user-event/dist/utils";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function Weather() {
-  return <div>"HI from react</div>;
+  let [loaded, upLoaded] = useState(false);
+  let [search, newSearch] = useState("");
+  let [message, newMessage] = useState("");
+
+  function showWeather(response) {
+    upLoaded(true);
+    newMessage(response.data.main.temp);
+  }
+  function hadelSubmit(event) {
+    event.preventDefault();
+    let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
+    let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}&units=metric`;
+    axios.get(apiurl).then(showWeather);
+  }
+  function handelChange(event) {
+    newSearch(event.target.value);
+  }
+
+  let form = (
+    <form className="row g-3 align-items-center" onSubmit={hadelSubmit}>
+      <div className="col-auto">
+        <input
+          type="search"
+          placeholder="Search a city"
+          onChange={handelChange}
+        />
+      </div>
+      <div className="col-auto">
+        <button type="submit" className="btn btn-primary btn-lg ">
+          Current location
+        </button>
+      </div>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+
+        <h2>
+          The temperature in {search} is : {Math.round(message)}°C
+        </h2>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
