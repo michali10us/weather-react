1418a5767c26daa88aa91e3ce0a6d6c786d0b528
michal mechlovich <michali10us@gmail.com>
2023-10-23 16:20:39 -0700

    apiUrl for Forcast

--- src/Forcast.js
+++ src/Forcast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Forcast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
 export default function Forcast() {
+  function hadelRequest(response) {
+    console.log(response);
+  }
+
+  let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
+  let longitude = -9.1365919;
+  let latitude = 38.7077507;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}
+  &key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(hadelRequest);
+
   return (
     <div className="container Forcast">
       <div className="row">
