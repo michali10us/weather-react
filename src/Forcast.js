605a59e9e1821557390bee0edcb59f048315e9a9
michal mechlovich <michali10us@gmail.com>
2023-10-25 10:48:25 -0700

    useEffect()

--- src/Forcast.js
+++ src/Forcast.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Forcast.css";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 
 export default function Forcast(property) {
-  function hadelRequest(response) {
-    console.log(response.data);
-  }
+  console.log(property);
+  function hadelRequest(response) {}
 
-  let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
+  useEffect(() => {
+    let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
+    let longitude = property.coordinate.lon;
+    let latitude = property.coordinate.lat;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(hadelRequest);
+  }, []); // empty dependency array ensures this runs once when the component mounts
+
+  /*   let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
   let longitude = property.coordinate.lon;
   let latitude = property.coordinate.lat;
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(hadelRequest);
-
+ */
   return (
     <div className="container Forcast">
       <div className="row">
