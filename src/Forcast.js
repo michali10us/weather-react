24a293b358a5c0ff0880dc805c94136abf9378c8
michal mechlovich <michali10us@gmail.com>
2023-10-25 13:07:29 -0700

    icon

--- src/Forcast.js
+++ src/Forcast.js
@@ -1,40 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import "./Forcast.css";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 
 export default function Forcast(property) {
-  console.log(property);
-  function hadelRequest(response) {}
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState();
 
-  useEffect(() => {
+  function hadelRequest(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="container Forcast">
+        <div className="row">
+          <div className="col">
+            <div className="forcast-day">{forecast[0].time}</div>
+            <div>
+              <WeatherIcon code={"50d"} />
+            </div>
+            <div>
+              <span className="forcast-minTemp">
+                {forecast[0].temperature.minimum}
+              </span>
+              <span className="forcast-maxTemp">
+                {forecast[0].temperature.maximum}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
     let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
     let longitude = property.coordinate.lon;
     let latitude = property.coordinate.lat;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(hadelRequest);
-  }, []); // empty dependency array ensures this runs once when the component mounts
 
-  /*   let apiKey = "bd64o304c00cb1336373a57ft8094a9d";
-  let longitude = property.coordinate.lon;
-  let latitude = property.coordinate.lat;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(hadelRequest);
- */
-  return (
-    <div className="container Forcast">
-      <div className="row">
-        <div className="col">
-          <div className="forcast-day">Sun</div>
-          <div>
-            <WeatherIcon code={"01d"} />
-          </div>
-          <div>
-            <span className="forcast-minTemp">12°</span>
-            <span className="forcast-maxTemp">33°</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    return null;
+  }
 }
