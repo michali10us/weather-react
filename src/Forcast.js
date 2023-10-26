dd55e5fec766f3d459f087df01be28cbd686a33f
michal mechlovich <michali10us@gmail.com>
2023-10-26 13:20:40 -0700

    {forecast.map(function (forecastDaily, index)

--- src/Forcast.js
+++ src/Forcast.js
@@ -19,15 +19,20 @@ export default function Forcast(property) {
     return (
       <div className="container Forcast">
         <div className="row">
-          <div className="col">
-            <ForcastDaily dailyData={forecast[0]} />
-          </div>
+          {forecast.map(function (forecastDaily, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForcastDaily dailyData={forecastDaily} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
   } else {
     let apiKey = "1a2b7258ebd456c01aef9175dfe8b709";
-
     let longitude = property.coordinate.lon;
     let latitude = property.coordinate.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
